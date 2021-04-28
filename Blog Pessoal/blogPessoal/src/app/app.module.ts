c69c06d496dd4f448db56920664faa9ea82fff8f
João Pedro <Joaosilva8882@gmail.com>
2021-04-27 23:45:43 -0300

    Adicionado componentes menu, rodapé, cadastro e login

--- Blog Pessoal/blogPessoal/src/app/app.module.ts
+++ Blog Pessoal/blogPessoal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    LoginComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
