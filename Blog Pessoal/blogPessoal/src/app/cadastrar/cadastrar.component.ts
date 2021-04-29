5146507ee515a87a359c6431721ffd2fb6dc8c1e
João Pedro <Joaosilva8882@gmail.com>
2021-04-28 21:20:56 -0300

    Adicionando rotas entre login e cadastro

--- Blog Pessoal/blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ Blog Pessoal/blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
+  botaoClicado() {
+    let inputSenha = document.querySelector('#IdSenha')
+    let olho = document.querySelector('#eyes')
+
+    if(inputSenha?.getAttribute('type') == 'password') {
+      inputSenha.setAttribute('type', 'text')
+    } else {
+      inputSenha?.setAttribute('type', 'password')
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
